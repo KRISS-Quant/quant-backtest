f999aec17f04d02389000f93af5f71e57964eac6
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2024-04-21 01:43:56 +0800

    Actually executable branch

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const app = express();
+const port = 8080;
+
+const upload = multer();
+app.use(bodyParser.json());
+
+app.use("/src", express.static(__dirname + "/src"));
+app.get('/', function(req, res){
+  res.sendFile('index.html', { root: __dirname + "" } );
+});
+app.get('/algoMetadata', function(req, res){
+  res.sendFile('/algo/algoMetadata.json', { root: __dirname + "" } );
+});
+
+app.post('/algo/:algo', upload.single('file'), async (req, res) => {
+  try {
+    const algorithm = require(`./algo/${req.params.algo}`);
+    console.log("Received data: ", req.body);
+
+    res.json(await algorithm.response(req.body));
+  } catch {
+    console.error("Error loading algorithm module:", error);
+    res.status(500).send("Error loading algorithm");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
