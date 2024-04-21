17f552ea81521ad500d5316d51d6eeea1697c080
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2024-04-21 15:48:00 +0800

    Response display update

--- src/js/handleResponse.js
+++ src/js/handleResponse.js
@@ -1,5 +1,4 @@
 function handleResponse(data) {
-    document.getElementById("data-container").innerHTML += "<br>-----------------<br>";
     document.getElementById("data-container").innerHTML += JSON.stringify(data);
     document.getElementById("textbox").innerText = new Date();
 }
