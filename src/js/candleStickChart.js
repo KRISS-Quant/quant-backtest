2b675dfea57abdf910fa914ad589f06b567b5330
Tim Kang <inwookk0710@gmail.com>
2024-05-04 03:46:08 +0800

    update

--- src/js/candleStickChart.js
+++ src/js/candleStickChart.js
@@ -29,37 +29,95 @@ function generateChartOptions(){
         type: 'datetime'
       },
       yaxis: {
-        // To be fixed
         tooltip: {
-          enabled: true,
-          shared: true,
-          custom: [function({seriesIndex, dataPointIndex, w}) {
-            return w.globals.series[seriesIndex][dataPointIndex]
-          }, function({ seriesIndex, dataPointIndex, w }) {
-            var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
-            var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
-            var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
-            var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
-            return (
-              '<div class="apexcharts-tooltip-candlestick">' +
-              '<div>Open: <span class="value">' +
-              o +
-              '</span></div>' +
-              '<div>High: <span class="value">' +
-              h +
-              '</span></div>' +
-              '<div>Low: <span class="value">' +
-              l +
-              '</span></div>' +
-              '<div>Close: <span class="value">' +
-              c +
-              '</span></div>' +
-              '</div>'
-            )
-          }]
-        }
-      }
-      };
+            enabled: true,
+            custom: [function({seriesIndex, dataPointIndex, w}) {
+              return '';
+            }, function({ seriesIndex, dataPointIndex, w }) {
+              var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
+              var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
+              var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
+              var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
+              var startTime = w.globals.seriesX[seriesIndex][dataPointIndex]
+              var endTime = w.globals.seriesX[seriesIndex][dataPointIndex + 1];
+                if (endTime) {
+                    endTime = new Date(endTime).toLocaleString();
+                } else {
+                    endTime = 'End of Series';
+                }
+              return (
+                  '<div class="apexcharts-tooltip-candlestick">' +
+                  '<div>Start Time: <span class="value">' +
+                  new Date(startTime).toLocaleString() +
+                  '</span></div>' +
+                  '<div>End Time: <span class="value">' +
+                  endTime +
+                  '</span></div>' +
+                  '<div>Open: <span class="value">' +
+                  o +
+                  '</span></div>' +
+                  '<div>High: <span class="value">' +
+                  h +
+                  '</span></div>' +
+                  '<div>Low: <span class="value">' +
+                  l +
+                  '</span></div>' +
+                  '<div>Close: <span class="value">' +
+                  c +
+                  '</span></div>' +
+                  '</div>'
+              )
+            }]
+          }
+      },
+    //   tooltip: {
+    //     custom: [function({seriesIndex, dataPointIndex, w}) {
+    //       return '';
+    //     }, function({ seriesIndex, dataPointIndex, w }) {
+    //       var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
+    //       var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
+    //       var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
+    //       var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
+    //       var startTime = w.globals.seriesX[seriesIndex][dataPointIndex]
+    //       var endTime = w.globals.seriesX[seriesIndex][dataPointIndex + 1];
+    //         if (endTime) {
+    //             endTime = new Date(endTime).toLocaleString();
+    //         } else {
+    //             endTime = 'End of Series';
+    //         }
+    //         console.log('seriesIndex:', seriesIndex);
+    //         console.log('dataPointIndex:', dataPointIndex);
+    //         console.log('o:', o);
+    //         console.log('h:', h);
+    //         console.log('l:', l);
+    //         console.log('c:', c);
+    //         console.log('startTime:', startTime);
+    //         console.log('endTime:', endTime);    
+    //       return (
+    //           '<div class="apexcharts-tooltip-candlestick">' +
+    //           '<div>Start Time: <span class="value">' +
+    //           new Date(startTime).toLocaleString() +
+    //           '</span></div>' +
+    //           '<div>End Time: <span class="value">' +
+    //           endTime +
+    //           '</span></div>' +
+    //           '<div>Open: <span class="value">' +
+    //           o +
+    //           '</span></div>' +
+    //           '<div>High: <span class="value">' +
+    //           h +
+    //           '</span></div>' +
+    //           '<div>Low: <span class="value">' +
+    //           l +
+    //           '</span></div>' +
+    //           '<div>Close: <span class="value">' +
+    //           c +
+    //           '</span></div>' +
+    //           '</div>'
+    //       )
+    //     }]
+    //   }
+    };
       var count = 0;
       
       for (ticker in document.ticker_data){
@@ -76,7 +134,15 @@ function generateChartOptions(){
             });
         }
         count+=1;
+        // if (count == 2){
+        //     var data1 = document.ticker_data[ticker];
+        //     options.yaxis.push({
+        //         seriesName: 'second',
+        //         opposite: true, // this will display the y-axis on the right
+        //         min: Math.min(...data1.map(data => data.price)), // replace this with the minimum value of your second series
+        //         max: Math.max(...data1.map(data => data.price)) // replace this with the maximum value of your second series
+        //     });
+        //   }
       }
-      console.log(options.series);
     return options;
 }
